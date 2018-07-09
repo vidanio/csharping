ae7480ec22c2bab924eeb3d10d411df9bf18d65f
Vidanio <info@vidanio.com>
2018-07-09 12:26:18 +0200

    primeros paso de shuffle

--- shuffle/Form1.Designer.cs
+++ shuffle/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace shuffle
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.folderOpen = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtboxFolder = new System.Windows.Forms.TextBox();
+            this.txtboxFiles = new System.Windows.Forms.TextBox();
+            this.lblExcept = new System.Windows.Forms.Label();
+            this.btnShuffle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtboxFolder
+            // 
+            this.txtboxFolder.Location = new System.Drawing.Point(28, 21);
+            this.txtboxFolder.Name = "txtboxFolder";
+            this.txtboxFolder.ReadOnly = true;
+            this.txtboxFolder.Size = new System.Drawing.Size(278, 20);
+            this.txtboxFolder.TabIndex = 0;
+            this.txtboxFolder.Text = "(Get Folder)";
+            this.txtboxFolder.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtboxFolder_MouseClick);
+            // 
+            // txtboxFiles
+            // 
+            this.txtboxFiles.Location = new System.Drawing.Point(28, 47);
+            this.txtboxFiles.Multiline = true;
+            this.txtboxFiles.Name = "txtboxFiles";
+            this.txtboxFiles.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtboxFiles.Size = new System.Drawing.Size(713, 313);
+            this.txtboxFiles.TabIndex = 1;
+            // 
+            // lblExcept
+            // 
+            this.lblExcept.AutoSize = true;
+            this.lblExcept.Location = new System.Drawing.Point(28, 367);
+            this.lblExcept.Name = "lblExcept";
+            this.lblExcept.Size = new System.Drawing.Size(35, 13);
+            this.lblExcept.TabIndex = 2;
+            this.lblExcept.Text = "label1";
+            // 
+            // btnShuffle
+            // 
+            this.btnShuffle.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnShuffle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShuffle.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnShuffle.Location = new System.Drawing.Point(344, 20);
+            this.btnShuffle.Name = "btnShuffle";
+            this.btnShuffle.Size = new System.Drawing.Size(79, 21);
+            this.btnShuffle.TabIndex = 3;
+            this.btnShuffle.Text = "Shuffle";
+            this.btnShuffle.UseVisualStyleBackColor = false;
+            this.btnShuffle.Click += new System.EventHandler(this.btnShuffle_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnShuffle);
+            this.Controls.Add(this.lblExcept);
+            this.Controls.Add(this.txtboxFiles);
+            this.Controls.Add(this.txtboxFolder);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shuffle";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderOpen;
+        private System.Windows.Forms.TextBox txtboxFolder;
+        private System.Windows.Forms.TextBox txtboxFiles;
+        private System.Windows.Forms.Label lblExcept;
+        private System.Windows.Forms.Button btnShuffle;
     }
 }
 
--- shuffle/Form1.cs
+++ shuffle/Form1.cs
@@ -6,14 +6,56 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace shuffle
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        // para guardar la coleccion de ficheros encontrados en el directorio
+        private IEnumerable<string> files, shuffle; // files esta en el orden real del directorio, shuffle es la desordenacion
+
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void txtboxFolder_MouseClick(object sender, MouseEventArgs e)
+        {
+            lblExcept.Text = "";
+            if (folderOpen.ShowDialog() == DialogResult.OK)
+            {
+                txtboxFolder.Text = folderOpen.SelectedPath;
+                try
+                {
+                    files = Directory.EnumerateFiles(txtboxFolder.Text, "*.mp3", SearchOption.AllDirectories);
+                    txtboxFiles.Clear();
+                    foreach (string currentFile in files)
+                    {
+                        txtboxFiles.AppendText(string.Format("{0}\r\n", currentFile));
+                    }
+                }
+                catch (Exception except)
+                {
+                    lblExcept.Text = except.Message;
+                }
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            lblExcept.Text = "";
+        }
+
+        private void btnShuffle_Click(object sender, EventArgs e)
+        {
+            var rnd = new Random();
+            shuffle = files.OrderBy(item => rnd.Next());
+            txtboxFiles.Clear();
+            foreach (string currentFile in shuffle)
+            {
+                txtboxFiles.AppendText(string.Format("{0}\r\n", currentFile));
+            }
         }
     }
 }
