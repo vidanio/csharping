4addf95a441e19061556a40035e0b940d0a706a9
Vidanio <info@vidanio.com>
2018-07-16 16:10:36 +0200

    filing casi completo

--- filing/Form1.Designer.cs
+++ filing/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace filing
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,34 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.webClient1 = new System.Net.WebClient();
+            this.StatusDown = new System.Windows.Forms.StatusStrip();
+            this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LimpiarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AbrirToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.GuardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SalirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TxtBox = new System.Windows.Forms.TextBox();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.OpenFileDlg = new System.Windows.Forms.OpenFileDialog();
+            this.SaveFileDlg = new System.Windows.Forms.SaveFileDialog();
+            this.StatusDown.SuspendLayout();
+            this.MainMenu.SuspendLayout();
             this.SuspendLayout();
             // 
-            // webClient1
+            // StatusDown
             // 
-            this.webClient1.BaseAddress = "";
-            this.webClient1.CachePolicy = null;
-            this.webClient1.Credentials = null;
-            this.webClient1.Encoding = ((System.Text.Encoding)(resources.GetObject("webClient1.Encoding")));
-            this.webClient1.Headers = ((System.Net.WebHeaderCollection)(resources.GetObject("webClient1.Headers")));
-            this.webClient1.QueryString = ((System.Collections.Specialized.NameValueCollection)(resources.GetObject("webClient1.QueryString")));
-            this.webClient1.UseDefaultCredentials = false;
+            this.StatusDown.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            this.StatusDown.Location = new System.Drawing.Point(0, 495);
+            this.StatusDown.Name = "StatusDown";
+            this.StatusDown.Size = new System.Drawing.Size(755, 22);
+            this.StatusDown.TabIndex = 2;
             // 
-            // Form1
+            // MainMenu
+            // 
+            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem});
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Size = new System.Drawing.Size(755, 24);
+            this.MainMenu.TabIndex = 0;
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LimpiarToolStripMenuItem,
+            this.AbrirToolStripMenuItem1,
+            this.GuardarToolStripMenuItem,
+            this.SalirToolStripMenuItem});
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.nuevoToolStripMenuItem.Text = "Archivo";
+            // 
+            // LimpiarToolStripMenuItem
+            // 
+            this.LimpiarToolStripMenuItem.Name = "LimpiarToolStripMenuItem";
+            this.LimpiarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.LimpiarToolStripMenuItem.Text = "&Limpiar";
+            this.LimpiarToolStripMenuItem.Click += new System.EventHandler(this.LimpiarToolStripMenuItem_Click);
+            // 
+            // AbrirToolStripMenuItem1
+            // 
+            this.AbrirToolStripMenuItem1.Name = "AbrirToolStripMenuItem1";
+            this.AbrirToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.AbrirToolStripMenuItem1.Text = "A&brir fichero";
+            this.AbrirToolStripMenuItem1.Click += new System.EventHandler(this.AbrirToolStripMenuItem1_Click);
+            // 
+            // GuardarToolStripMenuItem
+            // 
+            this.GuardarToolStripMenuItem.Name = "GuardarToolStripMenuItem";
+            this.GuardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.GuardarToolStripMenuItem.Text = "&Guardar";
+            this.GuardarToolStripMenuItem.Click += new System.EventHandler(this.GuardarToolStripMenuItem_Click);
+            // 
+            // SalirToolStripMenuItem
+            // 
+            this.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem";
+            this.SalirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.SalirToolStripMenuItem.Text = "&Salir";
+            this.SalirToolStripMenuItem.Click += new System.EventHandler(this.SalirToolStripMenuItem_Click);
+            // 
+            // TxtBox
+            // 
+            this.TxtBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TxtBox.Location = new System.Drawing.Point(0, 24);
+            this.TxtBox.Multiline = true;
+            this.TxtBox.Name = "TxtBox";
+            this.TxtBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.TxtBox.Size = new System.Drawing.Size(755, 471);
+            this.TxtBox.TabIndex = 1;
+            this.TxtBox.TextChanged += new System.EventHandler(this.TxtBox_TextChanged);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(48, 17);
+            this.StatusLabel.Text = "(no file)";
+            // 
+            // OpenFileDlg
+            // 
+            this.OpenFileDlg.Filter = "Text File|*.txt";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(629, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(755, 517);
+            this.Controls.Add(this.TxtBox);
+            this.Controls.Add(this.StatusDown);
+            this.Controls.Add(this.MainMenu);
+            this.MainMenuStrip = this.MainMenu;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Filing";
+            this.StatusDown.ResumeLayout(false);
+            this.StatusDown.PerformLayout();
+            this.MainMenu.ResumeLayout(false);
+            this.MainMenu.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Net.WebClient webClient1;
+        private System.Windows.Forms.StatusStrip StatusDown;
+        private System.Windows.Forms.MenuStrip MainMenu;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem LimpiarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AbrirToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem GuardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SalirToolStripMenuItem;
+        private System.Windows.Forms.TextBox TxtBox;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.OpenFileDialog OpenFileDlg;
+        private System.Windows.Forms.SaveFileDialog SaveFileDlg;
     }
 }
 
--- filing/Form1.cs
+++ filing/Form1.cs
@@ -6,14 +6,81 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace filing
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private string currentFile = "";
+        private bool filesaved = true;
+
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void LimpiarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!filesaved)
+            {
+                if (MessageBox.Show("Estás seguro de limpiar todo sin antes guardar?", "Alerta", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+                {
+                    currentFile = "";
+                    TxtBox.Clear();
+                    StatusLabel.Text = "(no file)";
+                }
+            }
+            else
+            {
+                currentFile = "";
+                TxtBox.Clear();
+                StatusLabel.Text = "(no file)";
+            }
+        }
+
+        private void SalirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void TxtBox_TextChanged(object sender, EventArgs e)
+        {
+            filesaved = false;
+        }
+
+        private void AbrirToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDlg.ShowDialog() == DialogResult.OK)
+            {
+                currentFile = OpenFileDlg.FileName;
+                StatusLabel.Text = currentFile;
+                TxtBox.Text = File.ReadAllText(currentFile);
+            }
+        }
+
+        private void GuardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string filetosave = "";
+
+            if (currentFile == "")
+            {
+                if (SaveFileDlg.ShowDialog() == DialogResult.OK)
+                {
+                    filetosave = SaveFileDlg.FileName;
+                    currentFile = filetosave;
+                    StatusLabel.Text = filetosave;
+                }
+            }
+            else
+            {
+                filetosave = currentFile;
+            }
+
+            if (filetosave != "")
+            {
+                File.WriteAllText(filetosave, TxtBox.Text);
+            }
         }
     }
 }
