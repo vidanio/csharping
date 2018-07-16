5b0307e64a472a7b65295ba56027f4f0f4287b73
Vidanio <info@vidanio.com>
2018-07-16 19:34:13 +0200

    crypto avanzado

--- crypto/Form1.Designer.cs
+++ crypto/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace crypto
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtboxFolderIn = new System.Windows.Forms.TextBox();
+            this.txtboxFolderOut = new System.Windows.Forms.TextBox();
+            this.txtListIn = new System.Windows.Forms.TextBox();
+            this.txtListOut = new System.Windows.Forms.TextBox();
+            this.btnCrypt = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.fldbrwDlg = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // txtboxFolderIn
+            // 
+            this.txtboxFolderIn.Location = new System.Drawing.Point(58, 86);
+            this.txtboxFolderIn.Name = "txtboxFolderIn";
+            this.txtboxFolderIn.ReadOnly = true;
+            this.txtboxFolderIn.Size = new System.Drawing.Size(452, 20);
+            this.txtboxFolderIn.TabIndex = 0;
+            this.txtboxFolderIn.Text = "(choose input folder)";
+            this.txtboxFolderIn.Click += new System.EventHandler(this.txtboxFolderIn_Click);
+            // 
+            // txtboxFolderOut
+            // 
+            this.txtboxFolderOut.Location = new System.Drawing.Point(562, 86);
+            this.txtboxFolderOut.Name = "txtboxFolderOut";
+            this.txtboxFolderOut.ReadOnly = true;
+            this.txtboxFolderOut.Size = new System.Drawing.Size(452, 20);
+            this.txtboxFolderOut.TabIndex = 0;
+            this.txtboxFolderOut.Text = "(choose output folder)";
+            this.txtboxFolderOut.Click += new System.EventHandler(this.txtboxFolderOut_Click);
+            // 
+            // txtListIn
+            // 
+            this.txtListIn.Location = new System.Drawing.Point(58, 112);
+            this.txtListIn.Multiline = true;
+            this.txtListIn.Name = "txtListIn";
+            this.txtListIn.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtListIn.Size = new System.Drawing.Size(452, 381);
+            this.txtListIn.TabIndex = 1;
+            // 
+            // txtListOut
+            // 
+            this.txtListOut.Location = new System.Drawing.Point(562, 112);
+            this.txtListOut.Multiline = true;
+            this.txtListOut.Name = "txtListOut";
+            this.txtListOut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtListOut.Size = new System.Drawing.Size(452, 381);
+            this.txtListOut.TabIndex = 1;
+            // 
+            // btnCrypt
+            // 
+            this.btnCrypt.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnCrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrypt.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCrypt.Location = new System.Drawing.Point(461, 499);
+            this.btnCrypt.Name = "btnCrypt";
+            this.btnCrypt.Size = new System.Drawing.Size(133, 36);
+            this.btnCrypt.TabIndex = 2;
+            this.btnCrypt.Text = "Crypt/Decrypt";
+            this.btnCrypt.UseVisualStyleBackColor = false;
+            this.btnCrypt.Click += new System.EventHandler(this.btnCrypt_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(461, 553);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(37, 13);
+            this.lblMsg.TabIndex = 3;
+            this.lblMsg.Text = "(none)";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1093, 620);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.btnCrypt);
+            this.Controls.Add(this.txtListOut);
+            this.Controls.Add(this.txtListIn);
+            this.Controls.Add(this.txtboxFolderOut);
+            this.Controls.Add(this.txtboxFolderIn);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crypto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtboxFolderIn;
+        private System.Windows.Forms.TextBox txtboxFolderOut;
+        private System.Windows.Forms.TextBox txtListIn;
+        private System.Windows.Forms.TextBox txtListOut;
+        private System.Windows.Forms.Button btnCrypt;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.FolderBrowserDialog fldbrwDlg;
     }
 }
 
--- crypto/Form1.cs
+++ crypto/Form1.cs
@@ -6,14 +6,108 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace crypto
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private string folderIn = "", folderOut = "";
+        private bool encoding = true; // .mp3/.wma => .mp3.xxx/ .wma.xxx
+        private IEnumerable<string> files; // input files *.*
+        private List<string> inputfiles; // real audio files
+        private const byte KEYCODE = 0x45;
+
+        public MainForm()
         {
             InitializeComponent();
+            inputfiles = new List<string>();
+        }
+
+        private void txtboxFolderOut_Click(object sender, EventArgs e)
+        {
+            if (fldbrwDlg.ShowDialog() == DialogResult.OK)
+            {
+                folderOut = fldbrwDlg.SelectedPath;
+                txtboxFolderOut.Text = folderOut;
+            }
+        }
+
+        private void btnCrypt_Click(object sender, EventArgs e)
+        {
+            if ((files != null) && (folderOut != ""))
+            {
+                if (encoding)
+                { // encoding
+                    foreach (string file in inputfiles)
+                    {
+                        try
+                        {
+                            var bytes = File.ReadAllBytes(file);
+                            for (int i=0; i < bytes.Length; i++)
+                            {
+                                bytes[i] ^= KEYCODE;
+                            }
+                            File.WriteAllBytes(folderOut + @"\" + Path.GetFileName(file) + ".xxx", bytes);
+                            txtListOut.AppendText(string.Format("[OK] {0}\r\n", Path.GetFileName(file)));
+                        }
+                        catch
+                        {
+                            txtListOut.AppendText(string.Format("[ERR] {0}\r\n", Path.GetFileName(file)));
+                        }
+                    }
+                }
+                else
+                { // decoding
+                    foreach (string file in inputfiles)
+                    {
+                        try
+                        {
+                            var bytes = File.ReadAllBytes(file);
+                            for (int i = 0; i < bytes.Length; i++)
+                            {
+                                bytes[i] ^= KEYCODE;
+                            }
+                            File.WriteAllBytes(folderOut + @"\" + Path.GetFileNameWithoutExtension(file), bytes);
+                            txtListOut.AppendText(string.Format("[OK] {0}\r\n", Path.GetFileName(file)));
+                        }
+                        catch
+                        {
+                            txtListOut.AppendText(string.Format("[ERR] {0}\r\n", Path.GetFileName(file)));
+                        }
+                    }
+                }
+            }
+        }
+
+        private void txtboxFolderIn_Click(object sender, EventArgs e)
+        {
+            if (fldbrwDlg.ShowDialog() == DialogResult.OK)
+            {
+                folderIn = fldbrwDlg.SelectedPath;
+                txtboxFolderIn.Text = folderIn;
+                try
+                {
+                    files = Directory.EnumerateFiles(txtboxFolderIn.Text, "*.*", SearchOption.AllDirectories);
+                    int i = 0;
+                    foreach (string file in files)
+                    {
+                        if (Path.GetExtension(file) == ".mp3" || Path.GetExtension(file) == ".wma" || Path.GetExtension(file) == ".xxx")
+                        {
+                            inputfiles.Add(file);
+                            txtListIn.AppendText(string.Format("[{0}] {1}\r\n", ++i, file));
+                            encoding = (Path.GetExtension(file) == ".xxx") ? false: true;
+                        }
+                    }
+                    if (encoding) lblMsg.Text = "encription ON";
+                    else lblMsg.Text = "decription ON";
+                }
+                catch (Exception except)
+                {
+                    files = null;
+                    lblMsg.Text = string.Format("cannot open all directory tree ({0})", except.Message);
+                }
+            }
         }
     }
 }
