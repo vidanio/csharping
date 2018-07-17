6958d1ab84ea203d3a01aa790df408a9baaa7736
Isaac Martinez Lopez <0.o.IsaaC.o.0@hotmail.es>
2018-07-17 22:38:30 +0200

    Ishuffle: afalta del WMP

---
+++ ishuffle/Main.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ishuffle
+{
+    public partial class MainShuffle : Form
+    {   
+        
+        private IEnumerable<string> ficheros, mezcla; //Ordenado y Desordenado de los ficheros
+        private List<string> playlist; //Playlist de reproducción
+        //private bool stop = false; //Parada del WMP
+
+        public MainShuffle()
+        {
+            InitializeComponent();
+            playlist = new List<string>();
+            //axWMPro.PlayStateChange += axWMPro_PlayStateChange;
+        }
+
+        private void txtSelectDir_Click(object sender, EventArgs e)
+        {
+            lblInfoError.Text = "";
+            if (abrirDirectorios.ShowDialog() == DialogResult.OK)
+            {
+                listContentDir.Items.Clear();
+                txtSelectDir.Text = abrirDirectorios.SelectedPath;
+                //Ficheros MP3 o WMA
+                string[] extensions = { ".mp3", ".wma" };
+                //Obtenemos los ficheros; de ese directorio y sus sub-directorios
+                ficheros = Directory.EnumerateFiles(abrirDirectorios.SelectedPath, "*.*", SearchOption.AllDirectories)
+                    .Where(s => extensions.Any(ext => ext == Path.GetExtension(s)));
+                //Guardamos los ficheros en nuestro contenedor
+                foreach (string fichero in ficheros)
+                {
+                    listContentDir.Items.Add(fichero);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblInfoError.Text = "";
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (playlist.Count > 0)
+            {
+                btnPlay.Enabled = false;
+                axWMPro.URL = playlist[0];
+
+                //playingInfo.AppendText(string.Format("Play [{0}] {1}\r\n", index, playlist[index]));
+                //timer1.Start();
+            }
+        }
+
+        private void btnMezcla_Click(object sender, EventArgs e)
+        {
+            if (ficheros != null)
+            {
+                listContentDir.Items.Clear();
+                playlist.Clear();
+                var randw = new Random();
+                //Desordenamos los ficheros
+                mezcla = ficheros.OrderBy(val => randw.Next());
+                foreach (string m in mezcla)
+                {
+                    playlist.Add(m);
+                    listContentDir.Items.Add(m);
+                }
+            }
+        }
+    }
+}
