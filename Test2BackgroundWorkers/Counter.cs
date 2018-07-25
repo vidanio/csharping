35a5dddba030047bdfbaf2252fcc8c4722527d31
Vidanio <info@vidanio.com>
2018-07-25 19:09:32 +0200

    ejemplo TestHilo hecho esta vez con 2 bgworkers y una clase thread-safe

---
+++ Test2BackgroundWorkers/Counter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Test2BackgroundWorkers
+{
+    class Counter
+    {
+        private int counter;
+        private Mutex mutex; // mutex para asegurar counter en hilos
+
+        public Counter()
+        {
+            this.counter = 0;
+            mutex = new Mutex();
+        }
+
+        public int Increase()
+        {
+            int i;
+
+            mutex.WaitOne();
+            i = ++this.counter;
+            mutex.ReleaseMutex();
+
+            return i;
+        }
+
+        public int GetCounter()
+        {
+            int i;
+
+            mutex.WaitOne();
+            i = this.counter;
+            mutex.ReleaseMutex();
+
+            return i;
+        }
+    }
+}
+/*
+La clase Counter es sencilla, solamente contiene la variable a asegurar con su mutex, y los métodos publicos que nos interesan para implementar el ejemplo TestHilos
+*/
