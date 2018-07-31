37c6e021bb152addcf90f434240c7746ddfd573f
Vidanio <info@vidanio.com>
2018-07-31 15:34:49 +0200

    infinite loop on threads

---
+++ TestInfiniteLoop/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace winforms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bgHilo = new System.ComponentModel.BackgroundWorker();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.btnAction = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bgHilo
+            // 
+            this.bgHilo.WorkerReportsProgress = true;
+            this.bgHilo.WorkerSupportsCancellation = true;
+            this.bgHilo.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgHilo_DoWork);
+            this.bgHilo.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgHilo_ProgressChanged);
+            this.bgHilo.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgHilo_RunWorkerCompleted);
+            // 
+            // lblCount
+            // 
+            this.lblCount.Location = new System.Drawing.Point(12, 67);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(278, 23);
+            this.lblCount.TabIndex = 0;
+            this.lblCount.Text = "0";
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAction
+            // 
+            this.btnAction.Location = new System.Drawing.Point(113, 115);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(75, 23);
+            this.btnAction.TabIndex = 1;
+            this.btnAction.Text = "Start";
+            this.btnAction.UseVisualStyleBackColor = true;
+            this.btnAction.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(113, 156);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 271);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnAction);
+            this.Controls.Add(this.lblCount);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Infinite Loop";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker bgHilo;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button btnAction;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ TestInfiniteLoop/Form1.cs
@@ -0,0 +1,101 @@
+﻿/* Comentarios al final del codigo */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace winforms
+{
+    public partial class MainForm : Form
+    {
+        private int counter = 0;
+        private bool function = false; // false = start / true = stop
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void bgHilo_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bg = sender as BackgroundWorker;
+            int count = (int)e.Argument;
+
+            SpinWait sw = new SpinWait(); // espera multiprocesador (como un yield monoprocesador)
+            while (true) // bucle infinito
+            {
+                if (bg.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                count++;
+                bg.ReportProgress(0, count);
+                sw.SpinOnce(); // evita que el bucle infinito cuelgue el programa como en Go, si lo comentas se colgará mucho
+            }
+            e.Result = count;
+        }
+
+        private void bgHilo_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            counter = (int)e.UserState;
+            lblCount.Text = counter.ToString();
+        }
+
+        private void bgHilo_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null) // no se esperan excepciones pero por si acaso
+            {
+                MessageBox.Show(e.Error.Message, "Exception !!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (e.Cancelled)
+            {
+                // no hacemos nada, solo parar
+            }
+            else
+            {
+                // solo se puede salir del bucle infinito mediate cancelación por lo que no se espera que se ejecute esto nunca
+                counter = (int)e.Result;
+            }
+        }
+
+        private void btnAction_Click(object sender, EventArgs e)
+        {
+            if (function) // STOP
+            {
+                function = false;
+                btnAction.Text = "Start";
+                if (bgHilo.IsBusy)
+                    bgHilo.CancelAsync();
+            }
+            else // START
+            {
+                function = true;
+                btnAction.Text = "Stop";
+                if (!bgHilo.IsBusy)
+                    bgHilo.RunWorkerAsync(counter);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            if (!function)
+            {
+                counter = 0;
+                lblCount.Text = counter.ToString();
+            }
+        }
+    }
+}
+/*
+Este es un ejemplo interesante en el que usamos un hilo backgroundworker para gestionar un contador mostrado en un Lable del UI sin bloquear la ventana, corriendo asíncronamente
+al UI. Al usar en el hilo un bucle infinito, nos encontramos antes el típico problema del cuelgue debido a que este proceso se queda con todo el tiempo del scheduler, para ello
+podríamos usar un Thread.Sleep(1), que es la espera más pequeña de 1 segundo, pero es muy grande para un procesador que puede ir más rápido. Un procesador con 1 hyperthread, podría
+usar Thread.Yield(), pero en procesadores con varios hyperthreads, como backgroundworker trabaja en un threadpool, cambia de thread del OS, esto daría tb un cuelgue, por lo que
+desde .NET 4.0 existe la clase SpinWait para este fin. 
+*/
