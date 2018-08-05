331fe31bb30b5b46beb153411b59d9ad24963b0b
Isaac Martinez Lopez <0.o.IsaaC.o.0@hotmail.es>
2018-08-05 19:36:52 +0200

    Hilos Compartidos

---
+++ ithreads/HilosClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ithreads
+{
+    class HilosClass
+    {
+        private int cont;
+        private Mutex mtx; // mutex para asegurar counter en hilos
+
+        public HilosClass()
+        {
+            this.cont = 0;
+            mtx = new Mutex();
+        }
+
+        public int Incremento()
+        {
+            int i = 0;
+
+            mtx.WaitOne();//
+            i = ++this.cont;
+            mtx.ReleaseMutex();//
+            return i;
+        }
+
+        public string ParImpar()
+        {
+            string res = "";
+            if(this.cont % 2 == 0)
+            {
+                res = "ES PAR"; //PAR
+            }
+            else
+            {
+                res = "ES IMPAR"; //IMPAR
+            }
+            return res;
+        }
+    }
+}
