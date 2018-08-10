6d61abe967953de846a76664eddcaad5aa25a7cd
Vidanio <info@vidanio.com>
2018-08-10 10:34:05 +0200

    mejoras en el dibujado

--- Dibujar/Form1.cs
+++ Dibujar/Form1.cs
@@ -11,6 +11,8 @@ namespace Dibujar
 {
     public partial class MainForm : Form
     {
+        private bool paint = false;
+
         public MainForm()
         {
             InitializeComponent();
@@ -18,24 +20,29 @@ namespace Dibujar
 
         private void btnDibuja_Click(object sender, EventArgs e)
         {
-            Invalidate(); // redibuja toda la ventana
+            paint = true;
+            pictBox.Invalidate(); // redibuja el control pictBox
         }
 
         // se ejecuta cada vez q se redibuja la ventana
         private void pictBox_Paint(object sender, PaintEventArgs e)
         {
-            Graphics g = e.Graphics;
-            g.PageUnit = GraphicsUnit.Pixel; // medida por pixels
-            // usa TranslateTransform, RotateTransform y ScaleTransform para cambiar origen y ejes de coordenadas
-            // Creamos un Bitmap donde dibujar puntos
-            using (Bitmap bm = new Bitmap(pictBox.Width, pictBox.Height))
+            // evitamos que se dibuje antes de pulsar el boton Dibuja
+            if (paint)
             {
-                bm.SetPixel(5, 5, Color.Red);
-                bm.SetPixel(pictBox.Width - 6, pictBox.Height - 6, Color.Red);
-                // dibujamos el Bitmap creado
-                g.DrawImage(bm, 0, 0);
-                // dibujamos una elipse
-                g.DrawEllipse(Pens.Green, 0, 0, pictBox.Width - 1, pictBox.Height - 1);
+                Graphics g = e.Graphics;
+                g.PageUnit = GraphicsUnit.Pixel; // medida por pixels
+                                                 // usa TranslateTransform, RotateTransform y ScaleTransform para cambiar origen y ejes de coordenadas
+                                                 // Creamos un Bitmap donde dibujar puntos
+                using (Bitmap bm = new Bitmap(pictBox.Width, pictBox.Height))
+                {
+                    bm.SetPixel(5, 5, Color.Red);
+                    bm.SetPixel(pictBox.Width - 6, pictBox.Height - 6, Color.Red);
+                    // dibujamos el Bitmap creado
+                    g.DrawImage(bm, 0, 0);
+                    // dibujamos una elipse
+                    g.DrawEllipse(Pens.Green, 0, 0, pictBox.Width - 1, pictBox.Height - 1);
+                }
             }
         }
     }
