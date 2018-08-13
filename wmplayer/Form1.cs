d0fe93e6c22297fa3e1428f262c1c05c46abe4bb
Vidanio <info@vidanio.com>
2018-08-13 13:37:44 +0200

    register dll dinamically

--- wmplayer/Form1.cs
+++ wmplayer/Form1.cs
@@ -24,6 +24,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Security.Principal;
+using System.Diagnostics;
 
 namespace wmplayer
 {
@@ -191,6 +192,29 @@ namespace wmplayer
                 isAdmin = false;
             }
             return isAdmin;
+        }
+
+        public static void Registar_Dlls(string filePath)
+        {
+            try
+            {
+                //'/s' : Specifies regsvr32 to run silently and to not display any message boxes.
+                string fileinfo = "/s" + " " + "\"" + filePath + "\"";
+                Process reg = new Process();
+                //This file registers .dll files as command components in the registry.
+                reg.StartInfo.FileName = "regsvr32.exe";
+                reg.StartInfo.Arguments = fileinfo;
+                reg.StartInfo.UseShellExecute = false;
+                reg.StartInfo.CreateNoWindow = true;
+                reg.StartInfo.RedirectStandardOutput = true;
+                reg.Start();
+                reg.WaitForExit();
+                reg.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
