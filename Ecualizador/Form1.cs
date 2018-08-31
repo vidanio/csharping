82656ac2c9d0632659604d4e108df105f2120bbc
Vidanio <info@vidanio.com>
2018-08-31 18:34:30 +0200

    timer stop on sound done

--- Ecualizador/Form1.cs
+++ Ecualizador/Form1.cs
@@ -416,6 +416,7 @@ namespace Ecualizador
         private void audioDjStudio1_SoundDone(object sender, PlayerEventArgs e)
         {
             resetInfo();
+            timer1.Stop();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
