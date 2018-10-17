d0a35b8f6f753b547a5bded12390676f6a57da30
vidanio <info@vidanio.com>
2018-10-17 22:20:26 +0200

    dimensions

--- UIController/Form1.Designer.cs
+++ UIController/Form1.Designer.cs
@@ -35,6 +35,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 641);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1000, 680);
+            this.MinimumSize = new System.Drawing.Size(1000, 680);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "OneStream Controller";
