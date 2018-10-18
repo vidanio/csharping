80a1814f322e7274ceccd945320345f34dd21b2f
vidanio <info@vidanio.com>
2018-10-18 13:29:06 +0200

    login form

--- UIController/Form1.Designer.cs
+++ UIController/Form1.Designer.cs
@@ -28,24 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.controlDeDispositivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.separatorToolStripMenuItem = new System.Windows.Forms.ToolStripSeparator();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(984, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Location = new System.Drawing.Point(0, 619);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(984, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logInToolStripMenuItem,
+            this.controlDeDispositivoToolStripMenuItem,
+            this.separatorToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // logInToolStripMenuItem
+            // 
+            this.logInToolStripMenuItem.Name = "logInToolStripMenuItem";
+            this.logInToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.logInToolStripMenuItem.Text = "Log In";
+            // 
+            // controlDeDispositivoToolStripMenuItem
+            // 
+            this.controlDeDispositivoToolStripMenuItem.Name = "controlDeDispositivoToolStripMenuItem";
+            this.controlDeDispositivoToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.controlDeDispositivoToolStripMenuItem.Text = "Control de Dispositivo";
+            // 
+            // separatorToolStripMenuItem
+            // 
+            this.separatorToolStripMenuItem.Name = "separatorToolStripMenuItem";
+            this.separatorToolStripMenuItem.Size = new System.Drawing.Size(188, 6);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 641);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(1000, 680);
             this.MinimumSize = new System.Drawing.Size(1000, 680);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "OneStream Controller";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem controlDeDispositivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator separatorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
     }
 }
 
---
+++ UIController/Login.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace UIController
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.lblNumServer = new System.Windows.Forms.Label();
+            this.numServer = new System.Windows.Forms.NumericUpDown();
+            this.lblMail = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numServer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNumServer
+            // 
+            this.lblNumServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumServer.Image = ((System.Drawing.Image)(resources.GetObject("lblNumServer.Image")));
+            this.lblNumServer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblNumServer.Location = new System.Drawing.Point(16, 59);
+            this.lblNumServer.Name = "lblNumServer";
+            this.lblNumServer.Size = new System.Drawing.Size(109, 13);
+            this.lblNumServer.TabIndex = 0;
+            this.lblNumServer.Text = "Server Num ID:";
+            this.lblNumServer.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numServer
+            // 
+            this.numServer.Location = new System.Drawing.Point(136, 57);
+            this.numServer.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.numServer.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numServer.Name = "numServer";
+            this.numServer.Size = new System.Drawing.Size(61, 20);
+            this.numServer.TabIndex = 1;
+            this.numServer.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numServer.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lblMail
+            // 
+            this.lblMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMail.Image = ((System.Drawing.Image)(resources.GetObject("lblMail.Image")));
+            this.lblMail.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblMail.Location = new System.Drawing.Point(16, 83);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(68, 24);
+            this.lblMail.TabIndex = 0;
+            this.lblMail.Text = "E-mail:";
+            this.lblMail.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(136, 87);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(198, 20);
+            this.txtMail.TabIndex = 2;
+            // 
+            // lblPass
+            // 
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.Image = ((System.Drawing.Image)(resources.GetObject("lblPass.Image")));
+            this.lblPass.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblPass.Location = new System.Drawing.Point(16, 112);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(84, 30);
+            this.lblPass.TabIndex = 0;
+            this.lblPass.Text = "Password:";
+            this.lblPass.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(136, 118);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(198, 20);
+            this.txtPass.TabIndex = 2;
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.Image = ((System.Drawing.Image)(resources.GetObject("btnOK.Image")));
+            this.btnOK.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOK.Location = new System.Drawing.Point(168, 155);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(80, 23);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "   OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Image = ((System.Drawing.Image)(resources.GetObject("btnCancel.Image")));
+            this.btnCancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancel.Location = new System.Drawing.Point(254, 155);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(80, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ponga sus datos de Log In";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 202);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.numServer);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblMail);
+            this.Controls.Add(this.lblNumServer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Log In";
+            ((System.ComponentModel.ISupportInitialize)(this.numServer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumServer;
+        private System.Windows.Forms.NumericUpDown numServer;
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+    }
+}
