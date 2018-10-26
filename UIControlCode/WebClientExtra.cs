0fe2ccfdf48bcd348637f4a0cfb7d5c097b356a9
vidanio <info@vidanio.com>
2018-10-26 12:05:03 +0200

    upto 12 concurrent http connections

---
+++ UIControlCode/WebClientExtra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace UIControlCode
+{
+    class WebClientExtra : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest wr = base.GetWebRequest(address);
+            wr.Timeout = 3000; // timeout in milliseconds (ms) - 3 seconds
+            return wr;
+        }
+    }
+}
