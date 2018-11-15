ac16738f19154e4da45d4cb42e9f5096dd60d376
vidanio <info@vidanio.com>
2018-11-15 20:30:50 +0100

    netdevices

---
+++ NetDevice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace NetDevice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Retrive all network interface using GetAllNetworkInterface() method off NetworkInterface class.
+            NetworkInterface[] niArr = NetworkInterface.GetAllNetworkInterfaces();
+
+            Console.WriteLine("Retriving basic information of network.\n\n");
+
+            //Display all information of NetworkInterface using foreach loop.
+
+            foreach (NetworkInterface tempNetworkInterface in niArr)
+            {
+                Console.WriteLine("Network Description        : " + tempNetworkInterface.Description);
+                Console.WriteLine("Network ID                 : " + tempNetworkInterface.Id);
+                Console.WriteLine("Network Name               : " + tempNetworkInterface.Name);
+                Console.WriteLine("Network Interface Type     : " + tempNetworkInterface.NetworkInterfaceType.ToString());
+                Console.WriteLine("Network Operational Status : " + tempNetworkInterface.OperationalStatus);
+                Console.WriteLine("Network Spped              : " + tempNetworkInterface.Speed);
+                Console.WriteLine("Support Multicast          : " + tempNetworkInterface.SupportsMulticast);
+                Console.ReadLine();
+            }
+        }
+    }
+}
