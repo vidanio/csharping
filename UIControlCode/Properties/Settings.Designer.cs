3a06404b7f05943a427c37f50b6564f8635f2b87
vidanio <info@vidanio.com>
2018-12-04 15:50:22 +0100

    borrado completo de mDNS y local device

--- UIControlCode/Properties/Settings.Designer.cs
+++ UIControlCode/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace UIControlCode.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -56,126 +56,6 @@ namespace UIControlCode.Properties {
             }
             set {
                 this["LoginPass"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Source1E {
-            get {
-                return ((string)(this["Source1E"]));
-            }
-            set {
-                this["Source1E"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Source2E {
-            get {
-                return ((string)(this["Source2E"]));
-            }
-            set {
-                this["Source2E"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Smartkey1E {
-            get {
-                return ((string)(this["Smartkey1E"]));
-            }
-            set {
-                this["Smartkey1E"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Smartkey2E {
-            get {
-                return ((string)(this["Smartkey2E"]));
-            }
-            set {
-                this["Smartkey2E"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Smartkey1D {
-            get {
-                return ((string)(this["Smartkey1D"]));
-            }
-            set {
-                this["Smartkey1D"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Smartkey2D {
-            get {
-                return ((string)(this["Smartkey2D"]));
-            }
-            set {
-                this["Smartkey2D"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1")]
-        public int ServID1E {
-            get {
-                return ((int)(this["ServID1E"]));
-            }
-            set {
-                this["ServID1E"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1")]
-        public int ServID2E {
-            get {
-                return ((int)(this["ServID2E"]));
-            }
-            set {
-                this["ServID2E"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1")]
-        public int ServID1D {
-            get {
-                return ((int)(this["ServID1D"]));
-            }
-            set {
-                this["ServID1D"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1")]
-        public int ServID2D {
-            get {
-                return ((int)(this["ServID2D"]));
-            }
-            set {
-                this["ServID2D"] = value;
             }
         }
     }
