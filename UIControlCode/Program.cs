07a972028004a2d6e16cc1b7b0cdddea8c1241b2
vidanio <info@vidanio.com>
2018-12-06 06:56:45 +0100

    delete one instance restriction

--- UIControlCode/Program.cs
+++ UIControlCode/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Windows.Forms;
-// Needed by FirstInstance property
-using System.Reflection;
-using System.Threading;
 
 namespace UIControlCode
 {
@@ -14,30 +11,9 @@ namespace UIControlCode
         [STAThread]
         static void Main()
         {
-            if (FirstInstance)
-            {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new MainForm());
-            }
-            else
-            {
-                MessageBox.Show("Esta Aplicación ya esta corriendo !!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Application.Exit();
-            }
-        }
-
-        private static bool FirstInstance
-        {
-            get
-            {
-                bool created;
-                string name = Assembly.GetEntryAssembly().FullName;
-                // created will be True if the current thread creates and owns the mutex.
-                // Otherwise created will be False if a previous instance already exists.
-                Mutex mutex = new Mutex(true, name, out created);
-                return created;
-            }
         }
     }
 }
