c0ea65273f899afc9df90bf35106e85fd49c035f
vidanio <info@vidanio.com>
2019-02-13 18:51:41 +0100

    myIP example

---
+++ MyIP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MyIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string localIP;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            Console.WriteLine("IP local: " + localIP);
+            Console.ReadLine();
+        }
+    }
+}
